cb9f0ac19581ed3ec71ad9ba869b9a4a5697f84d
imran786dk <imran786dk@gmail.com>
2024-11-05 21:47:46 +0100

    Added WelcomeController

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome()  {
+		
+		return "Welcome to the demo app";
+	}
+
+}
