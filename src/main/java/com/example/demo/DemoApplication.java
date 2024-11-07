4aaf1198c9d57347a7e98322ba5cb1fac58232b2
Imran Ahmed <imran.ahmed@tietoevry.com>
2024-11-07 19:25:07 +0100

    Code clean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
